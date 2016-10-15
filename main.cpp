edfc108339f005b4e41d8f46af093f362287ce60
George <dyedpurple@googlemail.com>
2016-10-15 12:47:42 +0100

    Added main program
    At the moment, all this program is doing is initialising an array of
    five boids.

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+#include <SFML/Graphics.hpp>
+
+#include "boids.hpp"
+
+std::string vtos(sf::Vector2i v) {
+    std::string to_return;
+    to_return = std::to_string(v.x) + "i + " + std::to_string(v.y) + "j";
+    return to_return;
+}
+
+Boid **initialise_boids(unsigned int num_boids) {
+    // Set up all boids with random starting positions and velocities
+    // Returns a pointer to an array of pointers to boids, indexed by boid_ID
+
+    Boid **boid_array = new Boid*[num_boids];
+
+    for (unsigned int i = 0; i < num_boids - 1; i++) {
+        Boid new_boid = Boid(i);
+        boid_array[i] = &new_boid;
+    }
+
+    std::cout << "Returning pointer to boid_array\n";
+
+    return boid_array;
+}
+
+void calculate_distance_matrix(distance_info *boid_list) {
+    // Return an array containing the distance for all pairs of
+    // boids, indexed by their boid_ID
+}
+
+int main(int argc, char **argv) {
+    
+    unsigned int num_boids = 5;
+
+    initialise_boids(num_boids);
+
+    return 0;
+}
+

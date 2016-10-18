2c2e537509917e68a5789cf2639d70c7441e115f
George <dyedpurple@googlemail.com>
2016-10-18 11:54:16 +0100

    Dynamically allocate memory for each boid
    I was using static memory before, which was leading to all sorts of
    interesting bugs...

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ void initialise_boids(unsigned int num_boids, Boid **boid_array) {
     // Returns a pointer to an array of pointers to boids, indexed by boid_ID
 
     for (unsigned int i = 0; i < num_boids; i++) {
-        Boid new_boid = Boid(i);
-        boid_array[i] = &new_boid;
+        Boid *new_boid_ptr = new Boid(i);
+        boid_array[i] = new_boid_ptr;
     }
 
     std::cout << "Returning pointer to boid_array\n";

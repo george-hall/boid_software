57086ebd829f45ea415748822f1f3c5191ed1522
George <dyedpurple@googlemail.com>
2016-10-18 19:56:54 +0100

    Wrote random position and velocity generator fcn
    This function gives each boid a random position vector and a random
    velocity vector

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <random>
 
 #include <SFML/Graphics.hpp>
 
@@ -15,7 +16,28 @@ void initialise_boids(unsigned int num_boids, Boid **boid_array) {
     }
 
     std::cout << "Returning pointer to boid_array\n";
+}
 
+void set_random_attributes(unsigned int num_boids, Boid **boid_array,
+                           unsigned int board_width,
+                           unsigned int board_height) {
+
+    // Give each boid in boid_array a random position and velocity
+
+    // Set up random number generators
+    std::random_device rd; // Seed engine
+    std::mt19937 rng(rd()); // Use Mersenne Twister
+    std::uniform_int_distribution<int> rand_x_coord(0, board_width);
+    std::uniform_int_distribution<int> rand_y_coord(0, board_height);
+
+    std::uniform_int_distribution<int> rand_velocity(-5, 5);
+
+    for (unsigned int i = 0; i < num_boids; i++) {
+        Boid *boid_ptr = boid_array[i];
+        boid_ptr->set_position(rand_x_coord(rng), rand_y_coord(rng));
+        boid_ptr->set_velocity(rand_velocity(rng), rand_velocity(rng));
+        boid_array[i]->print();
+    }
 }
 
 void calculate_distance_matrix(distance_info *boid_list) {
@@ -27,9 +49,14 @@ int main(int argc, char **argv) {
 
     unsigned int num_boids = 5;
 
+    /* Both in px */
+    unsigned int board_width = 300;
+    unsigned int board_height = 300;
+
     Boid **boid_array = new Boid*[num_boids];
 
     initialise_boids(num_boids, boid_array);
+    set_random_attributes(num_boids, boid_array, board_width, board_height);
 
     delete[] boid_array;
 

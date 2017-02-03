33f184df4d5bd81326367395affa07df07ac469a
George <dyedpurple@googlemail.com>
2017-02-03 18:37:00 +0000

    Added script to plot correlations

---
+++ plot_corrs.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "usage: $0 <file>"
+    exit 1
+fi
+
+IN_FILE=$1
+OUT_GRAPH_NAME="graph.png"
+DATA_FILES=""
+TEMP_FILES=""
+
+for CORR_LEN in 10 30 50 70 90 110; do
+    AWK_COMMAND='{if ($4 == "'$CORR_LEN':") {print $5}}'
+    awk "$AWK_COMMAND" "$IN_FILE" > tmp
+    awk '{print NR "\t" $0}' tmp > "corr_length_"$CORR_LEN".tmp"
+    TEMP_FILES=$TEMP_FILES'corr_length_'$CORR_LEN'.tmp '
+    DATA_FILES=$DATA_FILES'"corr_length_'$CORR_LEN'.tmp" using 1:2, '
+    rm tmp
+done
+
+gnuplot -dislay -p <<EOF
+plot $DATA_FILES
+EOF
+
+rm $TEMP_FILES
+
+#xdg-open $OUT_GRAPH_NAME

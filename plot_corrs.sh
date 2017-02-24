b05726c8d837009a3552398af39de01576e1f89a
George <dyedpurple@googlemail.com>
2017-02-24 11:52:33 +0000

    Display 3D graphs

--- plot_corrs.sh
+++ plot_corrs.sh
@@ -10,17 +10,18 @@ IN_FILE=$1
 DATA_FILES=""
 TEMP_FILES=""
 
-for CORR_LEN in 10 30 50; do
-    AWK_COMMAND='{if ($4 == "'$CORR_LEN':") {print $5}}'
+# for CORR_LEN in 1 3 5 7 9 11 13; do
+for CORR_LEN in 3 13 23 33 43; do
+    AWK_COMMAND='{if ($1 == "'$CORR_LEN'") {print $2" "$1}}'
     awk "$AWK_COMMAND" "$IN_FILE" > tmp
     awk '{print NR "\t" $0}' tmp > "corr_length_"$CORR_LEN".tmp"
     TEMP_FILES=$TEMP_FILES'corr_length_'$CORR_LEN'.tmp '
-    DATA_FILES=$DATA_FILES'"corr_length_'$CORR_LEN'.tmp" using 1:2, '
+    DATA_FILES=$DATA_FILES'"corr_length_'$CORR_LEN'.tmp" using 1:3:2, '
     rm tmp
 done
 
 gnuplot -p <<EOF
-set yr [-2:2]; plot $DATA_FILES
+set xlabel "Iteration"; set ylabel "Length"; set zlabel "Correlation"; splot $DATA_FILES;
 EOF
 
 rm $TEMP_FILES

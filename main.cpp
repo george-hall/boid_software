fc4ab817a279de094230c3965ad6878291e3a8f4
George <dyedpurple@googlemail.com>
2017-03-16 14:56:58 +0000

    Print vision angle in quiet mode - fixed!

--- main.cpp
+++ main.cpp
@@ -608,7 +608,7 @@ int main(int argc, char **argv) {
                 }
             }
             else {
-                std::cout << "num_boids: " << args.num_boids << " max_x: " << args.max_x << " max_y: " << args.max_y << " iterations: " << iterations_cutoff << std::endl; break;
+                std::cout << "num_boids: " << args.num_boids << " max_x: " << args.max_x << " max_y: " << args.max_y << " vision_angle: " << args.vision_angle << " iterations: " << iterations_completed - 50 << std::endl; break;
             }
         }
         iterations_completed++;

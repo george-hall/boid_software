6b21c77d05765ec2855c5a830d2572316f33bfdf
George <dyedpurple@googlemail.com>
2017-03-23 13:50:41 +0000

    Store corr_len as variable

--- main.cpp
+++ main.cpp
@@ -590,7 +590,8 @@ int main(int argc, char **argv) {
         update_fluctuations(args, boid_array, fluctuations_matrix);
         if (args.print_corrs) {
             //print_correlations(args, fluctuations_matrix, dist_matrix, boid_array);
-            std::cout << iterations_completed << " " << calc_corr_length(args, fluctuations_matrix, dist_matrix, boid_array) << std::endl;
+            corr_len = calc_corr_length(args, fluctuations_matrix, dist_matrix, boid_array);
+            std::cout << iterations_completed << " " << corr_len << std::endl;
         }
 
         float polarisation = calculate_polarisation(boid_array, args.num_boids);
